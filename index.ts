25f0a458fa9abe8b777403c13bd0b4f538783128
Safwan Shaheer <devorein00@gmail.com>
2021-07-11 18:42:29 +0600

    Integrated firebase

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
 import discord from "discord.js";
+import firebase from 'firebase/app';
 require('dotenv').config()
 
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+}
+
 const client = new discord.Client()
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+console.log(firebaseApp);
 
 client.on('ready', async ()=>{
   if(client.user)
@@ -14,4 +26,4 @@ client.on('message', async (msg)=>{
   }
 })
 
-client.login(process.env.TOKEN);
+client.login(process.env.DISCORD_TOKEN);
