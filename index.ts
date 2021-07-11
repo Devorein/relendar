ff1c0353d9ba47551af722b638294ce332798c41
Safwan Shaheer <devorein00@gmail.com>
2021-07-11 20:17:13 +0600

    Integrated commander

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import { QuerySnapshot } from "@google-cloud/firestore";
+import { Command } from 'commander';
 import discord from "discord.js";
 import admin from 'firebase-admin';
 import { ITask } from "./types";
 require('dotenv').config()
+const program = new Command();
 
 admin.initializeApp({
   credential: admin.credential.cert({
@@ -22,27 +24,42 @@ client.on('ready', async ()=>{
     console.log(`Logged in as ${client.user.tag}`)
 });
 
+async function getTasks(msg: discord.Message){
+  const docs = await tasksCollection.get() as QuerySnapshot<ITask>;
+  const messages = docs.docs.map((doc, index)=>{
+    const data = doc.data();
+    return `${index + 1}. **${data.course}** - ${data.task} - ${data.date}`
+  })
+  msg.reply("\n"+messages.join("\n"))
+}
+
+async function setTasks(course: string, task: string, date: string, msg: discord.Message){
+  await tasksCollection.doc(`${course}.${task}`).set({
+    course, task, date
+  });
+  msg.reply(`Created ${course}.${task}`)
+}
+
 client.on('message', async (msg)=>{
   const authorized = msg.member?.roles.cache.has("774226395454373928");
   const isBot = msg.author.bot;
 
   if(authorized && !isBot){
     if(msg.content.startsWith("!")){
-      const content = msg.content.slice(1);
-      const [command, ...chunks] = content.split(" ");
-      if(command === "set"){
-        const [course, task, date] = chunks;
-        await tasksCollection.doc(`${course}.${task}`).set({
-          course, task, date
-        });
-        msg.reply(`Created ${course}.${task}`)
-      } else if(command === "get"){
-        const docs = await tasksCollection.get() as QuerySnapshot<ITask>;
-        msg.reply(docs.docs.map((doc, index)=>{
-          const data = doc.data();
-          return `${index + 1}. ${data.course} - ${data.task} - ${data.date}\n`
-        }))
-      }
+      program
+        .command('get')
+        .description("Get tasks")
+        .action(()=>getTasks(msg))
+        .command("set")
+        .description("Set a new task")
+        .argument('<course>', 'Name of the course')
+        .argument('<task>', 'Task of the course')
+        .argument('<date>', 'Last date of the task')
+        .action((course, task, date)=>setTasks(course, task, date, msg))
+
+      program.parse([msg.content.slice(1)], {
+        from: 'user'
+      });
     }
   } else if(!isBot){
     msg.reply(`You are not authorized. Get the Dev role first`)
