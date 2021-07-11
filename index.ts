ec67efc1f400b531fe8c7903eed67798ecdb185f
Safwan Shaheer <devorein00@gmail.com>
2021-07-11 19:56:32 +0600

    Bot only works if author has certain role

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
+import { QuerySnapshot } from "@google-cloud/firestore";
 import discord from "discord.js";
 import admin from 'firebase-admin';
+import { ITask } from "./types";
 require('dotenv').config()
 
 admin.initializeApp({
@@ -21,13 +23,29 @@ client.on('ready', async ()=>{
 });
 
 client.on('message', async (msg)=>{
-  if(msg.content.startsWith("!")){
-    const content = msg.content.slice(1);
-    const [course, task, date] = content.split(" ");
-    await tasksCollection.doc(`${course}.${task}`).set({
-      course, task, date
-    });
-    msg.reply(`Created ${course}.${task}`)
+  const authorized = msg.member?.roles.cache.has("774226395454373928");
+  const isBot = msg.author.bot;
+
+  if(authorized && !isBot){
+    if(msg.content.startsWith("!")){
+      const content = msg.content.slice(1);
+      const [command, ...chunks] = content.split(" ");
+      if(command === "set"){
+        const [course, task, date] = chunks;
+        await tasksCollection.doc(`${course}.${task}`).set({
+          course, task, date
+        });
+        msg.reply(`Created ${course}.${task}`)
+      } else if(command === "get"){
+        const docs = await tasksCollection.get() as QuerySnapshot<ITask>;
+        msg.reply(docs.docs.map((doc, index)=>{
+          const data = doc.data();
+          return `${index + 1}. ${data.course} - ${data.task} - ${data.date}\n`
+        }))
+      }
+    }
+  } else if(!isBot){
+    msg.reply(`You are not authorized. Get the Dev role first`)
   }
 })
 
