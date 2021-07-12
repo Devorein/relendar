e7474bd61cf19cff03426c834ca2de584318647b
Safwan Shaheer <devorein00@gmail.com>
2021-07-12 17:33:27 +0600

    Consecutive relative date addition feature

--- utils/fillDate.ts
+++ utils/fillDate.ts
@@ -1,23 +1,27 @@
-import moment, { DurationInputArg2 } from "moment";
-
-function addDate(date: string, char: string, unit: DurationInputArg2){
-  const startDate = new Date()
-  const daysToAdd = parseInt(date.replace(char,""))
-  const newDate = moment(startDate, "YYYY-MM-DD").add(daysToAdd, unit);
-  return newDate.format("YYYY-MM-DD")
-}
+import moment from "moment";
 
 export function fillDate(dateStr: string){
-  const [date, month, year] = dateStr.split("-");
-  if(date.includes("d")){
-    return addDate(date, "d", "days")
-  } else if(date.includes("m")){
-    return addDate(date, "m", "months")
-  } else if(date.includes("w")){
-    return addDate(date, "w", "weeks")
-  } else {
+  if(dateStr.includes("-")){
+    const [date, month, year] = dateStr.split("-");
     const current = new Date();
     const currentMonth = current.getMonth() + 1, currentYear = current.getFullYear();
     return `${year ?? currentYear}-${month ?? currentMonth}-${date}`;
+  } else {
+    const relativeChunks = dateStr.split("+");
+    let startDate = moment(new Date());
+    for (let index = 0; index < relativeChunks.length; index++) {
+      const relativeChunk = relativeChunks[index];
+      if(relativeChunk.includes("d")){
+        const daysToAdd = parseInt(relativeChunk.replace("d",""))
+        startDate = moment(startDate, "YYYY-MM-DD").add(daysToAdd, "days");
+      } else if(relativeChunk.includes("w")){
+        const daysToAdd = parseInt(relativeChunk.replace("w",""))
+        startDate = moment(startDate, "YYYY-MM-DD").add(daysToAdd, "weeks");
+      } else if(relativeChunk.includes("m")){
+        const daysToAdd = parseInt(relativeChunk.replace("m",""))
+        startDate = moment(startDate, "YYYY-MM-DD").add(daysToAdd, "months");
+      } 
+    }
+    return moment(startDate).format("YYYY-MM-DD");
   }
 }
