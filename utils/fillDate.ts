d700374873bacf56305e18670248e89c956db5a4
Safwan Shaheer <devorein00@gmail.com>
2021-07-12 17:27:53 +0600

    Added relative date, week and month feature

--- utils/fillDate.ts
+++ utils/fillDate.ts
@@ -1,12 +1,20 @@
-import moment from "moment";
+import moment, { DurationInputArg2 } from "moment";
+
+function addDate(date: string, char: string, unit: DurationInputArg2){
+  const startDate = new Date()
+  const daysToAdd = parseInt(date.replace(char,""))
+  const newDate = moment(startDate, "YYYY-MM-DD").add(daysToAdd, unit);
+  return newDate.format("YYYY-MM-DD")
+}
 
 export function fillDate(dateStr: string){
   const [date, month, year] = dateStr.split("-");
   if(date.includes("d")){
-    const startDate = new Date()
-    const daysToAdd = parseInt(date.replace("d",""))
-    const newDate = moment(startDate, "YYYY-MM-DD").add(daysToAdd, 'days');
-    return newDate.format("YYYY-MM-DD");
+    return addDate(date, "d", "days")
+  } else if(date.includes("m")){
+    return addDate(date, "m", "months")
+  } else if(date.includes("w")){
+    return addDate(date, "w", "weeks")
   } else {
     const current = new Date();
     const currentMonth = current.getMonth() + 1, currentYear = current.getFullYear();
