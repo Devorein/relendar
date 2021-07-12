3a77ffe45f07f0534b48c76743956d6b6b6903f6
Safwan Shaheer <devorein00@gmail.com>
2021-07-12 17:23:58 +0600

    Added feature to add relative date based on number of days

--- utils/fillDate.ts
+++ utils/fillDate.ts
@@ -1,6 +1,15 @@
+import moment from "moment";
+
 export function fillDate(dateStr: string){
   const [date, month, year] = dateStr.split("-");
-  const current = new Date();
-  const currentMonth = current.getMonth() + 1, currentYear = current.getFullYear();
-  return `${year ?? currentYear}-${month ?? currentMonth}-${date}`;
+  if(date.includes("d")){
+    const startDate = new Date()
+    const daysToAdd = parseInt(date.replace("d",""))
+    const newDate = moment(startDate, "YYYY-MM-DD").add(daysToAdd, 'days');
+    return newDate.format("YYYY-MM-DD");
+  } else {
+    const current = new Date();
+    const currentMonth = current.getMonth() + 1, currentYear = current.getFullYear();
+    return `${year ?? currentYear}-${month ?? currentMonth}-${date}`;
+  }
 }
